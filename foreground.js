a3a55e735f68380a7909f01a0e37bf0be5255cf5
Bram van Rijswijk <b.van.rijswijk@socialblue.com>
2021-08-19 20:21:18 +0200

    Supports skribbl now

--- foreground.js
+++ foreground.js
@@ -1,11 +1,8 @@
-let netherlands = document.querySelector("body > div.L3eUgb > div.o3j99.c93Gbe > div.uU7dJb").firstChild.nodeValue;
-let googleInput = document.querySelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input");
-let form = document.querySelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form");
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    const wordLength = netherlands.length;
+    let skribblInput = document.querySelector("#inputChat");
+    let form = document.querySelector("#formChat");
+    const wordLength = document.querySelector("#currentWord").innerText.length;
 
-    // pick word with same length
     const url = 'https://skribbl-word-guesser-api.test/api/words-with-specific-length';
     const options = {
         method: 'POST',
@@ -21,8 +18,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     fetch(url, options)
         .then(response => response.json())
         .then(data => {
-            googleInput.value = data[0].word;
-            form.submit()
+            skribblInput.value = data[0].word;
         });
 
     return true;
